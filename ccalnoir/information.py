047467dec709de2ccb64716b52e8b31e3fd868d3
edjuaro <edjuaro@gmail.com>
2019-04-02 14:37:24 -0700

    parsing GCT and CLS correctly

--- ccalnoir/information.py
+++ ccalnoir/information.py
@@ -339,8 +339,11 @@ def differential_gene_expression(
                     temp.readline()
                     classes = [int(i) for i in temp.readline().strip('\n').split(' ')]
                     classes = pd.Series(classes, index=data_df.columns)
+            except HTTPError:
+                classes = get_file_from_server(gene_pattern_url=gene_expression, file_type='CLS')
+                classes = pd.Series(classes, index=data_df.columns)
         except HTTPError:
-            classes = get_file_from_server(gene_pattern_url=gene_expression, file_type='GCT')
+            classes = get_file_from_server(gene_pattern_url=gene_expression, file_type='CLS')
             classes = pd.Series(classes, index=data_df.columns)
 
     gene_scores = make_match_panel(
